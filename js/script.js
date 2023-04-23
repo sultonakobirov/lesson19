b8507420dddc011131b8f7298e8f87b2f870ae5b
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-23 19:22:29 +0500

    "some fixes and feature adds"

--- js/script.js
+++ js/script.js
@@ -14,13 +14,41 @@ let intBright = doc.getElementById('bright')
 let price = doc.getElementById('price')
 let battery = doc.getElementById('battery')
 let longVersion = doc.getElementById('long-version')
-
+let airCon = doc.getElementById('air-con')
+let intColors = doc.querySelector('.int-colors')
+let velocity = doc.getElementById('vlct')
+let wheel = doc.getElementById('whl-rds')
+let temperature = doc.getElementById('tmprtr')
+let upButton = doc.querySelectorAll('.up')
+let downButton = doc.querySelectorAll('.down')
 
 //ext-int view changer
 let extIntFunction = (flex, none)=>{
     flex.style.display = 'flex'
     none.style.display = 'none'
 }
+//interior color changer
+function colorChanger(button, image){
+    button.onclick = () =>{
+        extIntImage.src = image
+    }
+}
+//equipment 
+function longVersionAirCon(airCon, longVersion){
+    onclick = () =>{
+        if (airCon.checked === true && longVersion.checked === true) {
+            price.textContent = "95, 990"
+        } else if(airCon.checked === true) {
+            price.textContent = "92, 990"
+        }  else if(longVersion.checked === true){
+            price.textContent = "93, 990"
+        }
+        if(airCon.checked === false && longVersion.checked === false){
+            price.textContent = "89, 990"
+        }
+    }
+}
+
 extIntChanger.addEventListener('click', () => {
     if(extIntChanger.checked === true){
         extIntImage.setAttribute('src', extImage)
@@ -33,23 +61,10 @@ extIntChanger.addEventListener('click', () => {
         extIntFunction(interior, exterior)
     }
 })
+upDown(upButton, downButton, wheel)
+colorChanger(intBlack, intImageBlack)
+colorChanger(intWhite, intImageWhite)
+colorChanger(intBright, intImageBright)
+longVersionAirCon(airCon, longVersion)
 
-
-//Interior 
-intBlack.onclick = () => {
-        extIntImage.src = intImageBlack
-}   
-intWhite.onclick = () => {
-    extIntImage.src = intImageWhite
-}
-intBright.onclick = () => {
-    extIntImage.src = intImageBright
-}
-longVersion.onclick = () => {
-    if(longVersion.checked == true){
-        price.textContent = '94, 990'
-    }else{
-        price.textContent = price.value
-    }
-}
 
