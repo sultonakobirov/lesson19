87d1450027391996ccebe1c9aa6ad8594da28f29
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-24 17:54:15 +0500

    "доработки"

--- js/script.js
+++ js/script.js
@@ -19,8 +19,27 @@ let intColors = doc.querySelector('.int-colors')
 let velocity = doc.getElementById('vlct')
 let wheel = doc.getElementById('whl-rds')
 let temperature = doc.getElementById('tmprtr')
-let upButton = doc.querySelectorAll('.up')
-let downButton = doc.querySelectorAll('.down')
+let upButtonWhl = doc.getElementById('upwhl')
+let downButtonWhl = doc.getElementById('downwhl')
+
+let inPrice = 89
+let wheelContent = parseInt(wheel.textContent)
+let maxRadius = 21
+let minRadius = 17
+
+let maxTemp = 55
+let minTemp = -30
+let upButtonTmp = doc.getElementById('uptmp')
+let downButtonTmp = doc.getElementById('downtmp')
+temperatureContent = temperature.textContent
+temperatureContent = parseInt(temperatureContent)
+
+let maxVel = 255
+let minVel = 0
+let upButtonVlc = doc.getElementById('upvlct')
+let downButtonVlc = doc.getElementById('downvlct')
+let velocityContent = parseInt(velocity.textContent)
+let batteryContent = parseInt(battery.textContent)
 
 //ext-int view changer
 let extIntFunction = (flex, none)=>{
@@ -37,15 +56,15 @@ function colorChanger(button, image){
 function longVersionAirCon(airCon, longVersion){
     onclick = () =>{
         if (airCon.checked === true && longVersion.checked === true) {
-            price.textContent = "95, 990"
+            price.textContent = inPrice + 6
         } else if(airCon.checked === true) {
-            price.textContent = "92, 990"
+            price.textContent = inPrice + 3
         }  else if(longVersion.checked === true){
-            price.textContent = "93, 990"
+            price.textContent = inPrice + 4
         }
         if(airCon.checked === false && longVersion.checked === false){
-            price.textContent = "89, 990"
-        }
+            price.textContent = inPrice
+        } 
     }
 }
 
@@ -61,7 +80,36 @@ extIntChanger.addEventListener('click', () => {
         extIntFunction(interior, exterior)
     }
 })
-upDown(upButton, downButton, wheel)
+
+
+
+function upDownFunc(up, down, innum, min, max, indicontent, dist, distcontent){
+    up.onclick = () => {
+        innum = innum+1
+        if (innum >= 80) {
+            distcontent = distcontent - 2
+            dist.textContent = distcontent
+        }
+        indicontent.textContent = innum 
+            if (innum >= max) {
+                up.onclick = null
+            }
+    }
+    down.onclick = () => {
+        innum =innum - 1 
+        indicontent.textContent = innum
+        if (innum <= min) {
+            down.onclick = null
+        }
+        if (innum <= 0) {
+            distcontent = distcontent - 10
+            dist.textContent = distcontent
+        }
+    }
+}
+upDownFunc(upButtonVlc, downButtonVlc, velocityContent, minVel, maxVel, velocity, battery, batteryContent)
+upDownFunc(upButtonTmp, downButtonTmp, temperatureContent, minTemp, maxTemp, temperature, battery, batteryContent)
+upDownFunc(upButtonWhl, downButtonWhl, wheelContent, minRadius, maxRadius, wheel, null)
 colorChanger(intBlack, intImageBlack)
 colorChanger(intWhite, intImageWhite)
 colorChanger(intBright, intImageBright)
